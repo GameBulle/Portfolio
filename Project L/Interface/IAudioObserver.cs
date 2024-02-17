dc5456defbd5bf3b928d6fb90991825311185f8a
GameBulle <160034276+GameBulle@users.noreply.github.com>
2024-02-17 23:03:44 +0900

    Delete Project L directory

--- Project L/Interface/IAudioObserver.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IAudioObserver
-{
-    public void UpdateMasterVolume(float masterVolume);
-    public void UpdateSFXVolume(float SFXVolume);
-}
--- Project L/Interface/IPoolingObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IPoolingObject
-{
-    public void SetPosition(Vector3 pos);
-    public void SetAngle(Vector3 angle);
-}
