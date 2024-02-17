892c2b92a9da86c5518354a0e8f65dec2e5e59cb
GameBulle <160034276+GameBulle@users.noreply.github.com>
2024-02-18 00:51:48 +0900

    Add files via upload

---
+++ Project L/UI/Slot/LoadSlot.cs
@@ -0,0 +1,68 @@
+using TMPro;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class LoadSlot : MonoBehaviour
+{
+    [SerializeField] GameObject empty;
+    [SerializeField] TextMeshProUGUI level;
+    [SerializeField] TextMeshProUGUI saveTime;
+    [SerializeField] TextMeshProUGUI playTime;
+
+    int index;
+
+    public void Initilize(int index)
+    {
+        this.index = index;
+    }
+
+
+    public void UpdateLoadSlotUI(PlayerSaveData data)
+    {
+        empty.SetActive(false);
+        StringBuilder sb = new();
+
+        sb.Append("Level : ");
+        sb.Append(data.Level);
+        level.text = sb.ToString();
+
+        sb.Clear();
+        sb.Append("마지막 시간 : ");
+        sb.Append(data.SaveTime);
+        saveTime.text = sb.ToString();
+
+        sb.Clear();
+        sb.Append("플레이 시간 : ");
+        sb.Append(data.PlayTime);
+        playTime.text = sb.ToString();
+    }
+
+    public void Click()
+    {
+        if (empty.activeSelf == true)
+            return;
+        SoundMgr.Instance.PlaySFXAudio("GameStart");
+
+        if (DataMgr.Instance.LoadData(index))
+            InterfaceMgr.Instance.GameStart();
+        else
+            return;
+    }
+
+    private void OnEnable()
+    {
+        StringBuilder sb = new();
+        sb.Append(DataMgr.Instance.Path);
+        sb.Append(index);
+  
+        if (File.Exists(sb.ToString()))
+        {
+            string loadFile = File.ReadAllText(sb.ToString());
+            PlayerSaveData loadData = JsonUtility.FromJson<PlayerSaveData>(loadFile);
+            UpdateLoadSlotUI(loadData);
+        }
+        else
+            empty.SetActive(true);
+    }
+}
